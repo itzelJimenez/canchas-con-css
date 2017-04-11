a2b8b96d2ecce31d664edc451205fb25f87c27ac
itzel <itzeljimenez.do@gmail.com>
2017-04-11 00:27:58 -0500

    Cambio exitoso de cancha/ Falta terminar diseños

--- assets/main.js
+++ assets/main.js
@@ -1,21 +1,31 @@
+//Botones
 var futbol = document.getElementById('botonFut');
 var basquet = document.getElementById('botonBasq');
 var tenis = document.getElementById('botonTen');
-
+//Secciones
 var canchaFut = document.getElementById('futbolCancha');
+var canchaBasq = document.getElementById('basquetCancha');
+var canchaTenis = document.getElementById('tenisCancha');
+//Funciones
 ocultarCanchas();
 function ocultarCanchas(){
-	canchaFut.style.display = "none"
+	canchaFut.style.display = "none";
+	canchaBasq.style.display = "none";
+	canchaTenis.style.display = "none";
 }
 function aparecerFut(){
+	ocultarCanchas()
 	canchaFut.style.display = "block";
 }
-function mostrar(seccion){
-	seccion.display = "block";
+function aparecerBasq(){
+	ocultarCanchas()
+	canchaBasq.style.display = "block";
 }
-function ocultar(seccion){
-	seccion.display = "none";
+function aparecerTen(){
+	ocultarCanchas()
+	canchaTenis.style.display = "block";
 }
 //Eventos
-
 futbol.addEventListener("click", aparecerFut);
+basquet.addEventListener("click", aparecerBasq);
+tenis.addEventListener("click", aparecerTen);
