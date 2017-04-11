b798571293399996a6e181da1f1b64b6f254fa60
itzel <itzeljimenez.do@gmail.com>
2017-04-10 23:46:02 -0500

    maquetación de botones y enlazado de los mismos

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,14 @@
-//alert("baba de perico")
+var futbol = document.getElementById('botonFut');
+var basquet = document.getElementById('botonBasq');
+var tenis = document.getElementById('botonTen');
+
+
+function prueba(){
+	alert("Washo")
+}
+
+//Eventos
+
+futbol.addEventListener("click", prueba);
+basquet.addEventListener("click", prueba);
+tenis.addEventListener("click", prueba);
